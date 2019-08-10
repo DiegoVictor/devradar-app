f51d6a2fec9bcfbbfab6e9b127d3740b4458c1cf
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-08-10 14:24:29 -0300

    Igone socketio warning

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Routes from './routes';
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return (
