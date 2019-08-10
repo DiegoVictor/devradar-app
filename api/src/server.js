cae4e69f13b0a1c0143652030c1ca87c69df181e
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-08-10 13:04:55 -0300

    Install socketio

--- api/src/server.js
+++ api/src/server.js
@@ -1,18 +1,34 @@
 const Express = require('express');
-const Server = Express();
+const App = Express();
 const Mongoose = require('mongoose');
 const CORS = require('cors');
+const Server = require('http').Server(App);
+const SocketIo = require('socket.io')(Server);
 
 const { port } = require('./config/app');
 const { host, name, username, password } = require('./config/database');
+
+const connected = {};
+
+SocketIo.on('connection', socket => {
+  const { developer_id } = socket.handshake.query;
+  connected[developer_id] = socket.id;
+  
+});
 
 Mongoose.connect(
   `mongodb+srv://${username}:${password}@${host}/${name}?retryWrites=true&w=majority`,
   { useNewUrlParser: true }
 );
 
-Server.use(CORS());
-Server.use(Express.json());
-Server.use(require('./routes/main'));
+App.use((req, res, next) => {
+  req.io = SocketIo;
+  req.connected = connected;
+  return next();
+});
+
+App.use(CORS());
+App.use(Express.json());
+App.use(require('./routes/main'));
 
 Server.listen(port);
