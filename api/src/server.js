10a5ed0c020d4b18a45714f49232e610954d80e8
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-10-05 19:02:20 -0300

    Configure socketio server

--- api/src/server.js
+++ api/src/server.js
@@ -3,9 +3,27 @@ import Express from 'express';
 import Mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
+import socketio from 'socket.io';
+import http from 'http';
 import routes from './routes';
 
 const App = Express();
+const Server = http.Server(App);
+const io = socketio(Server);
+
+// Use Redis to relate users and socket id
+const connections = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connections[user_id] = socket.id;
+});
+
+App.use((req, res, next) => {
+  req.io = io;
+  req.connections = connections;
+  return next();
+});
 
 Mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -17,4 +35,4 @@ App.use(Express.json());
 App.use('/files', Express.static(path.resolve(__dirname, '..', 'uploads')));
 App.use(routes);
 
-App.listen(process.env.APP_PORT);
+Server.listen(process.env.APP_PORT);
