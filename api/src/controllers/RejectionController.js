647d6cd393a391391ef1c7f7f60f3cf87afbf1db
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-02 17:10:14 -0300

    Fix #4 Show bookings and permit cancel it

--- api/src/controllers/RejectionController.js
+++ api/src/controllers/RejectionController.js
@@ -1,16 +1,28 @@
+import { isAfter, subDays } from 'date-fns';
 import Booking from '../models/Booking';
 
 class RejectioController {
   async store(req, res) {
+    const { user_id: user } = req.headers;
     const { booking_id } = req.params;
     const booking = await Booking.findById(booking_id).populate('spot');
+
+    if (user) {
+      if (isAfter(new Date(), subDays(booking.date, 1))) {
+        return res.status(401).json({
+          error: 'You can only cancel bookings with 24 hours in advance',
+        });
+      }
+    }
 
     booking.approved = false;
     await booking.save();
 
-    const booking_user = req.connections[booking.user];
-    if (booking_user) {
-      req.io.to(booking_user).emit('booking_response', booking);
+    if (!user) {
+      const booking_user = req.connections[booking.user];
+      if (booking_user) {
+        req.io.to(booking_user).emit('booking_response', booking);
+      }
     }
 
     return res.json(booking);
