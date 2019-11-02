a33c7a8597f65306d9914364691ab93faf84bebd
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-02 18:18:59 -0300

    Check if request is spot owner or booking owner

--- api/src/controllers/RejectionController.js
+++ api/src/controllers/RejectionController.js
@@ -1,20 +1,33 @@
 import { isAfter, subDays } from 'date-fns';
+import Spot from '../models/Spot';
 import Booking from '../models/Booking';
 
 class RejectionController {
   async store(req, res) {
     const { user_id: user } = req.headers;
     const { booking_id } = req.params;
-    const booking = await Booking.findById(booking_id).populate('spot');
+
+    const spots = await Spot.find({ user });
+    const booking = await Booking.findOne({
+      _id: booking_id,
+      $or: [{ user }, { spot: spots.map(spot => spot._id) }],
+    }).populate('spot');
+
+    if (!booking) {
+      return res.status(401).json({
+        error:
+          "You did't request a booking to this spot or is not the spot owner",
+      });
+    }
 
     if (
       booking.user.equals(user) &&
       isAfter(new Date(), subDays(booking.date, 1))
     ) {
-        return res.status(401).json({
-          error: 'You can only cancel bookings with 24 hours in advance',
-        });
-      }
+      return res.status(401).json({
+        error: 'You can only cancel bookings with 24 hours in advance',
+      });
+    }
 
     booking.approved = false;
     await booking.save();
