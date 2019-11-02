0d8b0f186388bc15cdbf1b4d8afbcacb77fc5ef9
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-02 18:16:33 -0300

    Return user's pending booking requests

--- api/src/controllers/PendingController.js
+++ api/src/controllers/PendingController.js
@@ -1,12 +1,14 @@
 import Booking from '../models/Booking';
+import Spot from '../models/Spot';
 
 class PendingController {
   async index(req, res) {
     const { user_id: user } = req.headers;
+    const spots = await Spot.find({ user });
     const bookings = await Booking.find({
       approved: { $exists: false },
       date: { $gte: new Date() },
-      user,
+      spot: { $in: spots.map(spot => spot._id) },
     })
       .populate('user')
       .populate('spot');
