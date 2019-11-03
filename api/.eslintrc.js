e964bb5d0e3bb7cb392e66c63b05e2b9857d042d
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-03 09:30:04 -0300

    Install linters, nodemon and sucrase

---
+++ api/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base', 'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'camelcase': 'off',
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off'
+  },
+};
