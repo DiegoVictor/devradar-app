0ae8f01b6a57cf9b28706ee1f89c402a4e90acc0
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-11-03 09:34:54 -0300

    Fix lint warnings and errors

--- api/src/models/Developers.js
+++ api/src/models/Developers.js
@@ -3,30 +3,34 @@ import { Schema, model } from 'mongoose';
 export default model(
   'Developer',
   new Schema(
-  {
-    name: {
-      type: String,
-      required: true
-    },
-    user: {
-      type: String,
-      required: true
-    },
-    bio: String,
-    avatar: {
-      type: String,
-      required: true
-    },
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      user: {
+        type: String,
+        required: true,
+      },
+      bio: String,
+      avatar: {
+        type: String,
+        required: true,
+      },
 
-    likes: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Developer'
-    }],
-    dislikes: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Developer'
-    }]
-  },
-  { timestamps: true }
+      likes: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Developer',
+        },
+      ],
+      dislikes: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Developer',
+        },
+      ],
+    },
+    { timestamps: true }
   )
 );
