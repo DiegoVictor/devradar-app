6f8840688ddc0970e61a0b3417ff68f6d6cb3ee1
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-08 15:12:31 -0300

    Separate listen from the rest of configuration

---
+++ api/src/app.js
@@ -0,0 +1,37 @@
+import 'dotenv/config';
+import Express from 'express';
+import Mongoose from 'mongoose';
+import http from 'http';
+import cors from 'cors';
+import socketio from 'socket.io';
+
+import routes from './routes';
+
+const App = Express();
+const Server = http.Server(App);
+const io = socketio(Server);
+
+const connected = {};
+
+io.on('connection', socket => {
+  const { developer_id } = socket.handshake.query;
+  connected[developer_id] = socket.id;
+});
+
+Mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+App.use((req, res, next) => {
+  req.io = io;
+  req.connected = connected;
+  return next();
+});
+
+App.use(cors());
+App.use(Express.json());
+App.use(routes);
+
+export default App;
--- api/src/server.js
+++ api/src/server.js
@@ -1,36 +1,3 @@
-import 'dotenv/config';
-import Express from 'express';
-import Mongoose from 'mongoose';
-import http from 'http';
-import cors from 'cors';
-import socketio from 'socket.io';
-import routes from './routes';
+import App from './app';
 
-const App = Express();
-const Server = http.Server(App);
-const io = socketio(Server);
-
-const connected = {};
-
-io.on('connection', socket => {
-  const { developer_id } = socket.handshake.query;
-  connected[developer_id] = socket.id;
-});
-
-Mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-App.use((req, res, next) => {
-  req.io = io;
-  req.connected = connected;
-  return next();
-});
-
-App.use(cors());
-App.use(Express.json());
-App.use(routes);
-
-Server.listen(process.env.APP_PORT);
+App.listen(process.env.APP_PORT);
