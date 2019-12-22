577f64a340baacba6e819b9b05e2f43b035f9f7c
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-21 22:03:42 -0300

    Configure eslint rule

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   rules: {
     'prettier/prettier': 'error',
-    'camelcase': 'off',
-    'class-methods-use-this': 'off'
+    'class-methods-use-this': 'off',
+    'no-underscore-dangle': 'off',
   },
 };
