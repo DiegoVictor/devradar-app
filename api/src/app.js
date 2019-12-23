6289c4e684f23c1975eeecfadf182d1f8a3ba6cc
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-23 16:59:11 -0300

    Separate the app configuration from server boot

---
+++ api/src/app.js
@@ -0,0 +1,45 @@
+import 'dotenv/config';
+import Express from 'express';
+import Mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import socketio from 'socket.io';
+import http from 'http';
+import Sentry from '@sentry/node';
+
+import routes from './routes';
+
+const App = Express();
+export const Server = http.Server(App);
+const io = socketio(Server);
+
+if (process.env.LOG === '1') {
+  Sentry.init({ dsn: process.env.SENTRY_DSN });
+}
+
+// Use Redis to relate users and socket id
+const connections = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+  connections[user_id] = socket.id;
+});
+
+App.use((req, res, next) => {
+  req.io = io;
+  req.connections = connections;
+  return next();
+});
+
+Mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+App.use(cors());
+App.use(Express.json());
+App.use('/files', Express.static(path.resolve(__dirname, '..', 'uploads')));
+App.use(routes);
+
+export default App;
