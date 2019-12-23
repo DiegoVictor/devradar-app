9aee1f0acd9dd0c10c7f3f4eb20176867e6aaaca
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-23 16:57:00 -0300

    Configure jest

--- api/jest.config.js
+++ api/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   // automock: false,
 
   // Stop running tests after `n` failures
-  // bail: 0,
+  bail: 1,
 
   // Respect "browser" field in package.json when resolving modules
   // browser: false,
@@ -47,10 +47,10 @@ module.exports = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  // globalSetup: null,
+  globalSetup: './__tests__/setup.js',
 
   // A path to a module which exports an async function that is triggered once after all test suites
-  // globalTeardown: null,
+  globalTeardown: './__tests__/teardown.js',
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
