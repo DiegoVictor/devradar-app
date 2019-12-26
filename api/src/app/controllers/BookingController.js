776629f29493551d706af79ee80d7c2e1de76879
DiegoVictor <diegovictorgonzaga@gmail.com>
2019-12-26 19:48:09 -0300

    Improve population

--- api/src/app/controllers/BookingController.js
+++ api/src/app/controllers/BookingController.js
@@ -16,16 +16,19 @@ class BookingController {
     const { spot_id: spot } = req.params;
     const { date } = req.body;
 
-    const booking = await Booking.create({ user, spot, date });
+    let booking = await Booking.create({ user, spot, date });
 
-    booking.populate('spot').populate('user', (err, book) => {
-      const spot_owner = req.connections[book.spot.user];
-      if (spot_owner) {
-        req.io.to(spot_owner).emit('booking_request', booking);
-      }
-    });
+    booking = await booking
+      .populate('spot')
+      .populate('user')
+      .execPopulate();
 
-    return res.json(await booking);
+    const spot_owner = req.connections[booking.spot.user];
+    if (spot_owner) {
+      req.io.to(spot_owner).emit('booking_request', booking);
+    }
+
+    return res.json(booking);
   }
 }
 
