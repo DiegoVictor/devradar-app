faf854b4f23ee30974b4ef2d214f7689ec0fbef1
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-17 09:28:33 -0300

    Create routes

---
+++ app/src/routes.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './components/pages/Main';
+import Profile from './components/pages/Profile';
+
+export default createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: 'DevRadar',
+        },
+      },
+      Profile: {
+        screen: Profile,
+        navigationOptions: {
+          title: 'Perfil do Github',
+        },
+      },
+    },
+    {
+      defaultNavigationOptions: {
+        headerBackTitleVisible: false,
+        headerTintColor: '#FFF',
+        headerStyle: {
+          backgroundColor: '#7D40E7',
+        },
+      },
+    }
+  )
+);
