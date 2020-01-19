690734ade7fd5c9b912c3c76fbc6399557b2d082
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-19 13:33:57 -0300

    Configure main entry point

--- app/App.js
+++ app/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
 
-export default function App() {
+import './src/config/ReactotronConfig';
+import Routes from './src/routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
+      <Routes />
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
