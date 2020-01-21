5288325ea33b4a3b5086bb2f1c3c5f49d26e9d6e
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-21 19:31:01 -0300

    Fix #7 Validate developer register request

--- api/src/routes.js
+++ api/src/routes.js
@@ -4,12 +4,18 @@ import DeveloperConotroller from './app/controllers/DeveloperController';
 import LikeController from './app/controllers/LikeController';
 import DislikeController from './app/controllers/DislikeController';
 import MatchController from './app/controllers/MatchController';
+import DeveloperStore from './app/validators/DeveloperStore';
 
 const Route = Router();
 
+Route.post(
+  '/developers',
+  DeveloperStore,
+  DeveloperConotroller.store
+);
+
 Route.get('/developers', DeveloperConotroller.index);
 Route.get('/developers/:id', DeveloperConotroller.show);
-Route.post('/developers', DeveloperConotroller.store);
 
 Route.post('/developers/:liked_user_id/like', LikeController.store);
 Route.post('/developers/:disliked_user_id/dislike', DislikeController.store);
