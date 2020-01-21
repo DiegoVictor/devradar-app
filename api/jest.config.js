395ddea8e2b467e4ae683f5592a5d7c2271070e6
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-21 18:17:43 -0300

    Ignore file while creating coverage report

--- api/jest.config.js
+++ api/jest.config.js
@@ -27,9 +27,7 @@ module.exports = {
   coverageDirectory: '__tests__/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: ['src/app/services/Api.js'],
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text', 'lcov'],
