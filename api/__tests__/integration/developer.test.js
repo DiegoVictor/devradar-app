1f8f8e4d152481105bbd4d4d365b90009855462c
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-01-22 19:00:30 -0300

    Update developer resource tests

--- api/__tests__/integration/developer.test.js
+++ api/__tests__/integration/developer.test.js
@@ -1,9 +1,14 @@
 import request from 'supertest';
+import MockAdapter from 'axios-mock-adapter';
 import Mongoose from 'mongoose';
 
 import app from '../../src/app';
 import factory from '../utils/factories';
 import Developer from '../../src/app/models/Developer';
+import Api from '../../src/app/services/Api';
+import jwtoken from '../utils/jwtoken';
+
+const api_mock = new MockAdapter(Api);
 
 describe('Developer', () => {
   beforeEach(async () => {
@@ -16,9 +21,10 @@ describe('Developer', () => {
 
   it('should be able to get a list of developers', async () => {
     const [user] = await factory.createMany('Developer', 5);
+    const token = jwtoken(user.id);
     const response = await request(app)
       .get('/developers')
-      .set('user_id', user._id)
+      .set('Authorization', `Bearer ${token}`)
       .send();
 
     expect(Array.isArray(response.body)).toBe(true);
@@ -31,22 +37,27 @@ describe('Developer', () => {
 
   it('should not be able to get a list of developers', async () => {
     const [user] = await factory.createMany('Developer', 5);
+    const token = jwtoken(user.id);
     await user.delete();
 
     const response = await request(app)
       .get('/developers')
-      .set('user_id', user._id)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(400)
       .send();
 
-    expect(response.body).toStrictEqual({
-      error: 'User not exists',
+    expect(response.body).toMatchObject({
+      error: 'Bad Request',
+      message: 'Developer not exists',
     });
   });
 
   it('should be able to get a developer', async () => {
     const user = await factory.create('Developer');
+    const token = jwtoken(user.id);
     const response = await request(app)
       .get(`/developers/${user._id}`)
+      .set('Authorization', `Bearer ${token}`)
       .send();
 
     expect(response.body).toStrictEqual({
@@ -58,27 +69,31 @@ describe('Developer', () => {
   it('should be able to store a new developer', async () => {
     const { user, name, bio, avatar } = await factory.attrs('Developer');
 
-    axios.get = jest.fn(() => {
-      return new Promise(resolve => {
-        resolve({
-          data: {
-            name,
-            bio,
-            avatar_url: avatar,
-          },
-        });
-      });
-    });
+    api_mock.onGet().reply(200, { name, bio, avatar_url: avatar });
 
     const response = await request(app)
       .post('/developers')
       .send({ username: user });
 
     expect(response.body).toMatchObject({
-      name,
-      user: user.toLowerCase(),
-      bio,
-      avatar,
+      developer: {
+        name,
+        user: user.toLowerCase(),
+        bio,
+        avatar,
+      },
+      token: expect.any(String),
+    });
+  });
+
+  it('should not be able to store a new developer', async () => {
+    const response = await request(app)
+      .post('/developers')
+      .send();
+
+    expect(response.body).toMatchObject({
+      error: 'Bad Request',
+      message: 'Validation fails',
     });
   });
 });
