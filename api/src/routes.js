36b19b435f622c9eeee158dd4eb31b439ea4849d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-02 15:31:52 -0300

    Validate coming data

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,14 +2,17 @@ import { Router } from 'express';
 
 import DeveloperController from './app/controllers/DeveloperController';
 import SearchController from './app/controllers/SearchController';
+import DeveloperStore from './app/validators/DeveloperStore';
+import DeveloperUpdate from './app/validators/DeveloperUpdate';
+import DeveloperSearch from './app/validators/DeveloperSearch';
 
 const Route = Router();
 
 Route.get('/developers', DeveloperController.index);
-Route.post('/developers', DeveloperController.store);
-Route.put('/developers/:id', DeveloperController.update);
+Route.post('/developers', DeveloperStore, DeveloperController.store);
+Route.put('/developers/:id', DeveloperUpdate, DeveloperController.update);
 Route.delete('/developers/:id', DeveloperController.destroy);
 
-Route.get('/search', SearchController.index);
+Route.get('/search', DeveloperSearch, SearchController.index);
 
 export default Route;
