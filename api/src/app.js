b08812b4fff18d448ce7805201941914f1a81f10
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-02-03 20:20:33 -0300

    Separate server initiation from app configurations

---
+++ api/src/app.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+import Express from 'express';
+import Mongoose from 'mongoose';
+import http from 'http';
+import cors from 'cors';
+
+import routes from './routes';
+import { setupWebSocket } from './websocket';
+
+const App = Express();
+const Server = http.Server(App);
+
+setupWebSocket(Server);
+
+
+Mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+App.use(cors());
+App.use(Express.json());
+App.use(routes);
--- api/src/server.js
+++ api/src/server.js
@@ -1,25 +1,3 @@
-import 'dotenv/config';
-import Express from 'express';
-import Mongoose from 'mongoose';
-import http from 'http';
-import cors from 'cors';
-
-import routes from './routes';
-import { setupWebSocket } from './websocket';
-
-const App = Express();
-const Server = http.Server(App);
-
-setupWebSocket(Server);
-
-Mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-App.use(cors());
-App.use(Express.json());
-App.use(routes);
+import Server from './app';
 
 Server.listen(process.env.APP_PORT);
