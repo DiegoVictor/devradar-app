d6387e00cd21266ffb0575d4a96ce628c81c6676
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-05 18:40:58 -0300

    Move to subfolder

--- web/src/contexts/User/index.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-let user = {};
-if (typeof localStorage.tindev_user !== 'undefined') {
-  user = JSON.parse(localStorage.getItem('tindev_user'));
-}
-
-export const UserContext = createContext(user);
--- web/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import '~/config/ReactotronConfig';
-import Navigation from '~/routes';
-
-ReactDOM.render(<Navigation />, document.getElementById('root'));
