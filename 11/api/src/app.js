8d6bc2421d37427b58edad676149b68954bf601f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-24 19:23:38 -0300

    Configure cors

--- 11/api/src/app.js
+++ 11/api/src/app.js
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import Express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
 const App = Express();
+
+App.use(cors());
 App.use(Express.json());
 
 App.use(routes);
