9e0407df4f99bf1067670dd5507402e7909b6586
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-25 23:40:29 -0300

    Configure root import

--- 11/web/.eslintrc.js
+++ 11/web/.eslintrc.js
@@ -26,4 +26,11 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     camelcase: 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
