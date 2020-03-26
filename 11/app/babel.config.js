fddc64a117a6abaef9562317656a8c1690983654
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-26 18:21:51 -0300

    Configure eslint, prettier and root import

--- 11/app/babel.config.js
+++ 11/app/babel.config.js
@@ -1,6 +1,15 @@
-module.exports = function(api) {
+// eslint-disable-next-line func-names
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathSuffix: 'src',
+        },
+      ],
+    ],
   };
 };
