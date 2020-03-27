e68355386a6bf3a7a5d5595d00a6d67288564635
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-26 21:22:46 -0300

    Verify if the incident exists

--- 11/api/src/app/controllers/IncidentController.js
+++ 11/api/src/app/controllers/IncidentController.js
@@ -46,6 +46,14 @@ class IncidentController {
       .select('ong_id')
       .first();
 
+    if (!incident) {
+      return res.status(404).json({
+        error: {
+          message: 'Incident not found',
+        },
+      });
+    }
+
     if (incident.ong_id !== ong_id) {
       return res.status(401).json({
         error: {
