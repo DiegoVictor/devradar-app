d5f2f0e6544d943bf3e5b7d1ea311d3e1e8474a5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-27 20:45:29 -0300

    Move id creation logic to  its own file

--- 11/api/src/app/controllers/OngController.js
+++ 11/api/src/app/controllers/OngController.js
@@ -1,5 +1,5 @@
-import crypto from 'crypto';
 import connection from '../../database/connection';
+import generateUniqueId from '../../utils/generateUniqueId';
 
 class OngController {
   async index(_, res) {
@@ -9,7 +9,7 @@ class OngController {
 
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
     await connection('ongs').insert({ id, name, email, whatsapp, city, uf });
 
     return res.json({ id });
