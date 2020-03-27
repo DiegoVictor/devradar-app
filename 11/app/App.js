a290a64d8180c048f6b7488aad94b81d1d148da4
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-26 21:51:11 -0300

    Configure reactotron

--- 11/app/App.js
+++ 11/app/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import '~/config/ReactotronConfig';
 
 const styles = StyleSheet.create({
   container: {
---
+++ 11/app/src/config/ReactotronConfig.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  Reactotron.configure().useReactNative().connect().clear();
+}
