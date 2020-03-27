5551e0aa3c1241defd81cd2b113ba88488b26d01
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-27 20:45:45 -0300

    Update ong validation

--- 11/api/src/app/validators/Ong/Store.js
+++ 11/api/src/app/validators/Ong/Store.js
@@ -4,7 +4,7 @@ export default celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(12),
+    whatsapp: Joi.string().required().min(10).max(11),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   }),
