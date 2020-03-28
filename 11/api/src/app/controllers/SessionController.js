2577d1c457c5779d3a70c74ce3416cbe8d5d01da
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-28 16:17:00 -0300

    Return ong directly

--- 11/api/src/app/controllers/SessionController.js
+++ 11/api/src/app/controllers/SessionController.js
@@ -3,7 +3,6 @@ import connection from '../../database/connection';
 class SessionController {
   async store(req, res) {
     const { id } = req.body;
-
     const ong = await connection('ongs').where('id', id).select('name').first();
 
     if (!ong) {
@@ -14,7 +13,7 @@ class SessionController {
       });
     }
 
-    return res.json({ ong });
+    return res.json(ong);
   }
 }
 
