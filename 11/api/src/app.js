8524f1639e774faf3f87a9fb9f096479949c2eb7
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-29 14:51:03 -0300

    Install helmet

--- 11/api/src/app.js
+++ 11/api/src/app.js
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import Express from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
+import helmet from 'helmet';
 
 import routes from './routes';
 
 const App = Express();
 
+App.use(helmet());
 App.use(cors());
 App.use(Express.json());
 
