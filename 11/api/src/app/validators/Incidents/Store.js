98db51116c9dba0287d99bc6bb9964eff9eda6d7
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-29 15:18:49 -0300

    Create more validators and rename its folders

---
+++ 11/api/src/app/validators/Incidents/Store.js
@@ -0,0 +1,12 @@
+import { celebrate, Segments, Joi } from 'celebrate';
+
+export default celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().min(3).required(),
+    description: Joi.string().min(10).required(),
+    value: Joi.number().required(),
+  }),
+  [Segments.HEADERS]: Joi.object({
+    ong_id: Joi.string().required(),
+  }).unknown(),
+});
--- 11/api/src/routes.js
+++ 11/api/src/routes.js
@@ -5,20 +5,22 @@ import IncidentController from './app/controllers/IncidentController';
 import OngIncidentController from './app/controllers/OngIncidentController';
 import SessionController from './app/controllers/SessionController';
 
-import IncidentGet from './app/validators/Incident/Get';
-import IncidentDelete from './app/validators/Incident/Delete';
-import OngStore from './app/validators/Ong/Store';
+import IncidentGet from './app/validators/Incidents/Get';
+import IncidentDelete from './app/validators/Incidents/Delete';
+import IncidentStore from './app/validators/Incidents/Store';
+import OngStore from './app/validators/Ongs/Store';
 import OngIncidentsGet from './app/validators/OngIncidents/Get';
+import SessionStore from './app/validators/Sessions/Store';
 
 const Route = Router();
 
-Route.post('/sessions', SessionController.store);
+Route.post('/sessions', SessionStore, SessionController.store);
 
 Route.get('/ongs', OngController.index);
 Route.post('/ongs', OngStore, OngController.store);
 
 Route.get('/incidents', IncidentGet, IncidentController.index);
-Route.post('/incidents', IncidentController.store);
+Route.post('/incidents', IncidentStore, IncidentController.store);
 Route.delete('/incidents/:id', IncidentDelete, IncidentController.destroy);
 
 Route.get('/ong_incidents', OngIncidentsGet, OngIncidentController.index);
