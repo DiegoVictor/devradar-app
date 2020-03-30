cd36132655a40f214fa3f2ae805b23d77d2e4157
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-03-30 17:52:08 -0300

    Set a variable with the request url

--- 11/api/src/app.js
+++ 11/api/src/app.js
@@ -5,12 +5,15 @@ import { errors } from 'celebrate';
 import helmet from 'helmet';
 
 import routes from './routes';
+import ResourceUrl from './app/middlewares/ResourceUrl';
 
 const App = Express();
 
 App.use(helmet());
 App.use(cors());
 App.use(Express.json());
+
+App.use(ResourceUrl);
 
 App.use(routes);
 
