42d405de53812b2be1eec48e51450700d70c25ce
Diego Victor <diegovictorgonzaga@gmail.com>
2021-06-12 13:56:05 -0300

    Delete 10/web directory

--- 10/web/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import '~/config/ReactotronConfig';
-import Dashboard from '~/pages/Dashboard';
-
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
