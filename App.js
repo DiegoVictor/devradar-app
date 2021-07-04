ae67cc263e2a170341d7be3a4fe41934e72455b3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-04 14:19:50 -0300

    Rename component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import Navigation from './src';
-
+import Routes from './src/routes';
 
 export default () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7D40E7" />
-      <Navigation />
+      <Routes />
     </>
   );
 };
