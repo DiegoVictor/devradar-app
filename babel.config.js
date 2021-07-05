40381abdcfa14f357cc01643691d798ed60a74b8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-05 18:41:38 -0300

    Install/Configure dotenv import

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,17 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       [
+        'dotenv-import',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: false,
+        },
+      ],
+      [
         'babel-plugin-root-import',
         {
           rootPathSuffix: 'src',
