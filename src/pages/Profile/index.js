79b0760bafd0960664ecdd4902b54ae320416b2a
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-06 18:46:04 -0300

    Use arrow function

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Github } from './styles';
 
-export default function Profile({ navigation }) {
+export default ({ navigation }) => {
   const github_username = navigation.getParam('github_username');
   return <Github source={{ uri: `https://github.com/${github_username}` }} />;
 }
