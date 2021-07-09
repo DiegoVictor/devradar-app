3c5ba24ca6cd7079c2da38a7bab5753f4efc16e0
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-09 14:11:14 -0300

    Rename dev component test file

--- tests/components/Dev.spec.js
+++ tests/components/Dev.spec.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { render, fireEvent } from 'react-native-testing-library';
 
-import factory from '../../utils/factory';
+import { useNavigation } from '@react-navigation/native';
+import factory from '../utils/factory';
 import { Dev } from '~/components/Dev';
+
+jest.mock('@react-navigation/native');
 
 describe('Dev component', () => {
   it('should be able to create a map marker', async () => {
     const dev = await factory.attrs('Developer');
     const { getByText, getByTestId } = render(
-      <Dev testID="marker" dev={dev} navigation={{ navigate: jest.fn() }} />
+      <Dev testID="marker" dev={dev} />
     );
 
     expect(getByTestId('avatar')).toHaveProp('source', {
@@ -21,14 +24,16 @@ describe('Dev component', () => {
 
   it('should be able to navigate to user profile', async () => {
     const dev = await factory.attrs('Developer');
-    const navigate = jest.fn();
 
-    const { getByTestId } = render(<Dev dev={dev} navigation={{ navigate }} />);
+    const navigate = jest.fn();
+    useNavigation.mockReturnValue({ navigate });
+
+    const { getByTestId } = render(<Dev dev={dev} />);
 
     fireEvent.press(getByTestId('profile'));
 
     expect(navigate).toHaveBeenCalledWith('Profile', {
-      github_username: dev.github_username,
+      githubUsername: dev.github_username,
     });
   });
 });
