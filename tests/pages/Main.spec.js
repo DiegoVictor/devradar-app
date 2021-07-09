27d1b04bf04b815a33bef7a87e310434f4f1432c
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-07-09 14:13:36 -0300

    Rename main test file

--- tests/pages/Main.spec.js
+++ tests/pages/Main.spec.js
@@ -4,22 +4,28 @@ import faker from 'faker';
 import MockAdapter from 'axios-mock-adapter';
 import { create } from 'react-test-renderer';
 
-import { emit } from '__mocks__/socket.io-client';
-import factory from '../../utils/factory';
-import { callbacks, state } from '../../../__mocks__/react-native-maps';
+import { emit } from '../../mocks/socket.io-client';
+import factory from '../utils/factory';
+import { callbacks, state } from '../../mocks/react-native-maps';
 import Main from '~/pages/Main';
 import api from '~/services/api';
+import {
+  getCurrentPositionAsync,
+  requestPermissionsAsync,
+} from '~/../mocks/expo-location';
 
-const api_mock = new MockAdapter(api);
+jest.mock('@react-navigation/native');
 
 describe('Main page', () => {
+  const apiMock = new MockAdapter(api);
+
   it('should be able to search developers', async () => {
     const word = faker.lorem.word();
     const developers = await factory.attrsMany('Developer', 3, {
       techs: [word],
     });
 
-    api_mock.onGet('search').reply(200, developers);
+    apiMock.onGet('search').reply(200, developers);
 
     let root;
     await act(async () => {
@@ -42,6 +48,30 @@ describe('Main page', () => {
     });
   });
 
+  it('should not be able to search developers without provide techs', async () => {
+    const word = faker.lorem.word();
+    const developers = await factory.attrsMany('Developer', 3, {
+      techs: [word],
+    });
+
+    apiMock.onGet('search').reply(200, developers);
+
+    let root;
+    await act(async () => {
+      root = create(<Main />);
+    });
+
+    await act(async () => {
+      fireEvent.press(root.root.findByProps({ testID: 'search' }));
+    });
+
+    developers.forEach(dev => {
+      expect(
+        root.root.findAllByProps({ testID: `developer_${dev._id}` })
+      ).toStrictEqual([]);
+    });
+  });
+
   it('should be able to update user current position', async () => {
     const latitude = faker.address.latitude();
     const longitude = faker.address.longitude();
@@ -51,7 +81,7 @@ describe('Main page', () => {
     });
 
     await act(async () => {
-      callbacks.onRegionChangeComlpete({
+      callbacks.onRegionChangeComplete({
         latitude,
         longitude,
       });
@@ -75,11 +105,21 @@ describe('Main page', () => {
     });
 
     await act(async () => {
-      emit(developer);
+      emit('developer', developer);
     });
 
     expect(
       root.root.findByProps({ testID: `developer_${developer._id}` })
     ).toBeTruthy();
+  });
+
+  it('should not be able to get user current position', async () => {
+    requestPermissionsAsync.mockImplementation(() => ({ granted: false }));
+
+    await act(async () => {
+      create(<Main />);
+    });
+
+    expect(getCurrentPositionAsync).not.toHaveBeenCalled();
   });
 });
