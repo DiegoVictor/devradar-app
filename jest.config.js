2acf2a8062b6b3956619e3938bdcc4fd4c357954
DiegoVictor <diegovictorgonzaga@gmail.com>
2026-03-25 12:00:03 -0300

    Comment out setupFilesAfterEnv in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -127,7 +127,7 @@ module.exports = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  // setupFilesAfterEnv: [],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
